8092c4ff1e1a8a5e0120cef462431c5a67d66138
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-02-10 16:24:12 -0700

    Adds server class and test. Test never runs though...

--- lib/http_server.rb
+++ lib/http_server.rb
@@ -1,3 +1,9 @@
 require 'socket'
-tcp_server = TCPServer.new(9292)
-client = tcp_server.accept
+
+class Server
+  attr_reader :tcp_server, :client
+  def initialize(port)
+    @tcp_server = TCPServer.new(port)
+    @client = tcp_server.accept
+  end
+end
--- test/http_server_test.rb
+++ test/http_server_test.rb
@@ -1,3 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/htttp_server.rb'
+require './lib/http_server.rb'
+
+class ServerTest < Minitest::Test
+  def test_server_initializes
+    my_server = Server.new(9292)
+
+    assert_instance_of Server, my_server
+    assert_instance_of TCPServer, my_server.tcp_server
+  end
+end
